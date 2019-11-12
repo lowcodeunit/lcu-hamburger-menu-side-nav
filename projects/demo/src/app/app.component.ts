3a582c87ff8defaf8857e5948d06876313e08ccb
powhound4 <george.hatch@fathym.com>
2019-11-12 12:56:33 -0700

    add input for icon color

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -16,12 +16,16 @@ export class AppComponent implements OnInit {
 
   public Items: Array<NavLinkModel> = new Array<NavLinkModel>();
 
+  public Color: string;
+  public HoverColor: string;
   openedSubject = new Subject<boolean>();
 
   constructor(
     protected router: Router,
     protected activatedRoute: ActivatedRoute,
     protected overlayContainer: OverlayContainer) {
+      this.Color = 'black';
+      this.HoverColor = '#FF9849';
   }
 
   public ngOnInit(): void {
